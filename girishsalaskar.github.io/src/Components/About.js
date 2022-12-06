2a6e2ae4ee9df0b078eac69a99117b912818f5d7
Girish Salaskar <girishsalaskar812@gmail.com>
2022-12-06 17:49:04 +0530

    Typewriter effect and text added

--- girishsalaskar.github.io/src/Components/About.js
+++ girishsalaskar.github.io/src/Components/About.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Typewriter from 'typewriter-effect';
 
 const Home = () => {
   return (
@@ -10,6 +11,32 @@ const Home = () => {
         <div className="subheading mb-5">A/p Khardi, Tal. Pandharpur, Dist. Solapur · +91-9665-703062 ·
           <a href="mailto:girishsalaskar812@gmail.com">girishsalaskar812@gmail.com</a>
         </div>
+        <p style={{color:'rgba(0,0,0,0.7)'}}>
+          <Typewriter
+            onInit={(typewriter) => {
+              typewriter.typeString('I am a Coder')
+                .pauseFor(2500)
+                .deleteChars(7)
+                .typeString('an Investor')
+                .pauseFor(2500)
+                .deleteChars(11)
+                .typeString('a Trader')
+                .pauseFor(2500)
+                .deleteChars(8)
+                .typeString('Minimalist')
+                .pauseFor(2500)
+                .deleteChars(10)
+                // .typeString('Spiritual')
+                // .pauseFor(2500)
+                // .deleteChars(9)
+                .typeString('Music Lover')
+                .pauseFor(2500)
+                .deleteChars(11)
+                .start();
+            }}
+            options={{loop:true}}
+          />
+        </p>
         <p className="mb-5">I am experienced in leveraging agile frameworks to provide a robust synopsis for high level overviews. Iterative approaches to corporate strategy foster collaborative thinking to further the overall value proposition.</p>
         <ul className="list-inline list-social-icons mb-0">
           <li className="list-inline-item">
